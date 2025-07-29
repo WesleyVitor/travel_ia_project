b02846d8813ad0860cf4e17043c57f83c8826b0e
WesleyVitor <wesleydemorais@outlook.com.br>
2025-07-28 21:38:53 -0300

    feat: add duffel api like tool

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import os
 from typing import Annotated
 
-from langchain_core.messages import ToolMessage, HumanMessage
+from langchain_core.messages import ToolMessage, HumanMessage, AIMessage
 from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
@@ -10,7 +10,7 @@ from langchain_openai import ChatOpenAI
 
 from dotenv import load_dotenv
 
-from tools import airbnb_search
+from tools import airbnb_search, duffel_search
 
 load_dotenv()
 class AgentState(TypedDict):
@@ -21,23 +21,27 @@ class Agent:
     def __init__(self, model, tools):
         graph = StateGraph(AgentState)
         graph.add_node("llm", self.call_openai)
-        graph.add_node("call_airbnb_mcp", self.call_airbnb_mcp)
+        graph.add_node("call_tools", self.call_tools)
         graph.add_conditional_edges(
             "llm",
-            self.exists_action,
+            self.can_call_tools,
             {
-                True: "call_airbnb_mcp", 
+                True: "call_tools", 
                 False: END
             }
         )
-        graph.add_edge("call_airbnb_mcp", "llm")
+        graph.add_edge("call_tools", "llm")
+
         graph.add_edge(START, "llm")
         self.graph = graph.compile()
         self.tools = {t.name: t for t in tools}
         self.model = model.bind_tools(tools)
 
-    def exists_action(self, state: AgentState):
-        result = state['messages'][-1]
+    
+    def can_call_tools(self, state: AgentState):
+        """Check if the last message contains a tool call"""
+        ia_messages = [item for item in state['messages'] if isinstance(item, AIMessage)]
+        result = ia_messages[-1]
         return len(result.tool_calls) > 0
 
     def call_openai(self, state: AgentState):
@@ -45,19 +49,19 @@ class Agent:
         message = self.model.invoke(messages)
         return {'messages': [message]}
 
-    def call_airbnb_mcp(self, state: AgentState):
-        tool_calls = state['messages'][-1].tool_calls
+    def call_tools(self, state: AgentState):
+        
+        ia_messages = [item for item in state['messages'] if isinstance(item, AIMessage)]
+        tool_calls = ia_messages[-1].tool_calls
+
         results = []
         for tool in tool_calls:
-            print(f"Calling: {tool}")
-            if not tool['name'] in self.tools:      # check for bad tool name from LLM
-                print("\n ....bad tool name....")
-                result = "bad tool name, retry"  # instruct LLM to retry if bad
-            else:
-                result = self.tools[tool['name']].invoke(tool['args'])
-            results.append(ToolMessage(tool_call_id=tool['id'], name=tool['name'], content=str(result)))
+            print(f"Calling tool: {tool['name']} with args: {tool['args']}")
+            result = self.tools[tool['name']].invoke(tool['args'])
+            message = ToolMessage(tool_call_id=tool['id'], name=tool['name'], content=str(result))
+            results.append(message)
         print("Back to the model!")
-        return {'messages': results}
+        return {'messages': state["messages"] + results}
 
 
 llm = ChatOpenAI(
@@ -68,10 +72,15 @@ llm = ChatOpenAI(
 
 agent = Agent(
     model=llm,
-    tools=[airbnb_search]
+    tools=[airbnb_search, duffel_search]
 )
-
-messages = [HumanMessage(content="What the Airbnb lowest prices in Natal, RN ")]
+user_m = """
+Entre a data 2025-08-10 e a data 2025-08-13, eu quero passar 5 dias na cidade Miami. 
+Estou disposto a gastar o valor 400 em passagens e 1000 em hospedagem. 
+Para a hospedagem, quero ficar em um quarto com as seguintes características: com wifi. 
+Quais opções eu tenho?
+"""
+messages = [HumanMessage(content=user_m),]
 result = agent.graph.invoke({"messages": messages})
 print(result['messages'][-1].content)
 
--- tools.py
+++ tools.py
@@ -1,15 +1,86 @@
+import os
+import requests
 import asyncio
-
 from mcp_client import MCPClient
 
 from langchain.agents import tool
 from pydantic import BaseModel, Field
+from dotenv import load_dotenv
 
+load_dotenv()
 class AirbnbRequest(BaseModel):
     location: str = Field(..., description="The location to search for Airbnb listings.")
+
+class DuffelRequest(BaseModel):
+    origin: str = Field(..., description="The origin airport code.")
+    destination: str = Field(..., description="The destination airport code.")
+    departure_date: str = Field(..., description="The departure date in YYYY-MM-DD format.")
 
 @tool(args_schema=AirbnbRequest)
 def airbnb_search(location: str) -> str:
     """Search for Airbnb listings based on location."""
     client = MCPClient()
     return asyncio.run(client.airbnb_search(location))
+
+@tool(args_schema=DuffelRequest)
+def duffel_search(origin: str, destination: str, departure_date: str) -> str:
+    """Search for flights using Duffel API."""
+
+    headers = {
+        "Authorization": f"Bearer {os.getenv('DUFFEL_API_KEY')}",
+        "Accept": "application/json",
+        "Duffel-Version": "v2",
+        "Content-Type": "application/json",
+        "Accept-Encoding": "gzip"
+    }
+
+    data = {
+        "data": {
+            "slices": [
+                {
+                    "origin": origin,
+                    "destination": destination,
+                    "departure_date": departure_date
+                }
+            ],
+            "passengers": [{"type": "adult"}],
+            "max_connections": 0,
+            "cabin_class": "economy"
+        }
+    }
+
+    r = requests.post(
+        "https://api.duffel.com/air/offer_requests?return_offers=true&supplier_timeout=10000", 
+        headers=headers, 
+        json=data,
+        timeout=15
+    )
+    data = r.json()['data']
+
+    # for item in data['slices'][0]['destination']['airports']:
+    #     print(item)
+    #     print()
+
+    offers = []
+    for offer in data['offers']:
+        segment = offer['slices'][0]['segments'][0]
+
+        offer = (
+            f"ID: {offer['id']}\n"
+            f"Total: {offer['total_amount']}\n"
+            f"Currency: {offer['total_currency']}\n"
+            f"Documents: {offer['supported_passenger_identity_document_types']}\n"
+            f"Duration: {segment['duration']}\n"
+            f"Destination: {segment['destination']['city_name']}\n"
+            f"Airport: {segment['destination']['name']}\n"
+            f"Time Zone: {segment['destination']['time_zone']}\n"
+            f"Passengers: {segment['passengers']}\n"
+            f"Departure: {segment['departing_at']}\n"
+            f"Arrival: {segment['arriving_at']}\n"
+            #f"Aircraft: {segment['aircraft']['name'] if segment['aircraft'] else 'N/A'}\n"
+            f"--------------------"
+        )
+
+        offers.append(offer)
+    
+    return "\n".join(offers)
